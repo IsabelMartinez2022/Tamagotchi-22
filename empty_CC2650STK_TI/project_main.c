b1e95e16cbf8d3c6adf4265d2ffc4d28cbaaec01
Nieves Núñez <nievesnu@ucm.es>
2022-11-30 13:10:13 +0200

    almost

--- empty_CC2650STK_TI/project_main.c
+++ empty_CC2650STK_TI/project_main.c
@@ -24,7 +24,6 @@
 #include "Board.h"
 //#include "wireless/comm_lib.h"
 #include "sensors/opt3001.h"
-
 #include "sensors/mpu9250.h"
 
 /*Global variables*/
@@ -32,15 +31,6 @@ char csv[80], *token;
 const char s[2] = ",";
 char command_to_send[30];
 char sendingStates[10];
-
-typedef struct{
-    char name[12];
-    uint32_t pet;
-    uint32_t exercise;
-    uint32_t eat;
-    uint32_t daylight;
-    //time_t start;
-} Pet;
 
 typedef struct {
    uint8_t ax[8];
@@ -67,8 +57,10 @@ uint8_t uartBuffer[30];
 // Definition of the state machine
 enum state { WAITING=1, DATA_READY, READ_DATA, SELECT_BUTTON};
 enum state programState = WAITING;
+
+//Defect awake_mode
 enum myState {EAT_MODE, EXERCISE_MODE, PET_MODE, SLEEP_MODE};
-enum myState petState;
+enum myState petState; //used for jukebox
 
 //FLAG
 enum state{SEND, READING, DETECT};
@@ -130,152 +122,86 @@ static const I2CCC26XX_I2CPinCfg i2cMPUCfg = {
     .pinSCL = Board_I2C0_SCL1
 };
 
-void buttonFxn(PIN_Handle handle, PIN_Id pinId) {
-
-    //?
-    programState = SELECT_BUTTON;
-    if(programState == WAITING){
-        PIN_setOutputValue(ledHandle, Board_LED0, 0 );
-        programState = DATA_READY;
-    }else{
-        PIN_setOutputValue(ledHandle, Board_LED0, 1 );
-        programState = WAITING;
-    }
-}
-
-
 /* Task Functions */
 
-/* Shut Function */
-void shutFxn(PIN_Handle handle, PIN_Id pinId) {
-   PINCC26XX_setWakeup(buttonShut);
-   Power_shutdown(NULL,0);
-}
+//THIS TASK SENDS THE INFO TO THE BACKEND
+void sensorTaskAccGyro (UArg arg0, UArg arg1) {
 
-/* UART Function */
-void uartTaskFxn(UArg arg0, UArg arg1) {
-
-    char echo_msg [30];
-    UART_Handle uart;
-    UART_Params uartParams;
-
-    // Setup here UART connection as 9600,8n1
-    UART_Params_init(&uartParams);
-    uartParams.writeDataMode = UART_DATA_TEXT;
-    uartParams.readDataMode = UART_DATA_TEXT;
-    uartParams.baudRate = 9600;
-    uartParams.readMode = UART_MODE_CALLBACK;
-    //uartParams.readCallback = &uartFxn;
-    uartParams.dataLength = UART_LEN_8;
-    uartParams.parityType = UART_PAR_NONE;
-    uartParams.stopBits = UART_STOP_ONE;
-
-       uart = UART_open(Board_UART0, &uartParams);
-
-          if (uart == NULL) {
-             System_abort("Error opening the UART");
-          }
-
-    while (1) {
-
-        // Print out sensor data as string to debug window if the state is correct
-        // Remember to modify state
-
-        string_compare=strcmp(sendingState, "SEND");
-
-        if (string_compare==0){
-            UART_write(uart,command_to_send, strlen(command_to_send)+1);
-            Task_sleep(100000 / Clock_tickPeriod);
-            strcpy(sendingState, "DETECT");
-        }
-
-        // Just for sanity check for exercise, you can comment this out
-       // System_printf(" uartTask\n");
-        System_flush();
-
-        // Once per second, you can modify this
-        Task_sleep(100000 / Clock_tickPeriod);
-    }
-}
-
-/* Tamagotchi Task - the sensor task */
-
-void TamagotchiTask(UArg arg0, UArg arg1){
+    accValues acc;
+    gyroValues gyro;
 
     //I2C_Transaction i2cMessage;
+    I2C_Handle i2cMPU;
+    I2C_Params i2cMPUParams;
 
-        I2C_Handle i2c;
-        I2C_Params i2cParams;
+    I2C_Params_init(&i2cMPUParams);
+    i2cMPUParams.bitRate = I2C_400kHz;
+    i2cMPUParams.custom = (uintptr_t)&i2cMPUCfg;
 
-   // I2C_MPU_Transaction i2c_MPU_Message
+    // MPU power on
+    PIN_setOutputValue(hMpuPin,Board_MPU_POWER, Board_MPU_POWER_ON);
 
-        I2C_Handle i2cMPU;
-        I2C_Params i2cMPUParams;
+    // Wait 100ms for the MPU sensor to power up
+    Task_sleep(100000 / Clock_tickPeriod);
+    System_printf("MPU9250: Power ON\n");
+    System_flush();
 
-   // Init the i2c bus
-
-        I2C_Params_init(&i2cParams);
-        i2cParams.bitRate = I2C_400kHz;
-
-   // Init the i2cMPU bus
-
-        I2C_Params_init(&i2cMPUParams);
-        i2cMPUParams.bitRate = I2C_400kHz;
-        i2cMPU = I2C_open(Board_I2C_TMP, &i2cMPUParams);
-        if (i2cMPU == NULL) {
-            System_abort("Error Initializing i2cMPU\n");
-        }
-
-   // Open i2c connection
-        i2c = I2C_open(Board_I2C_TMP, &i2cParams);
-        if (i2c == NULL) {
-            System_abort("Error Initializing I2C\n");
-        }
-        else {
-            System_printf("I2C Initialized!\n");
-        }
-
-        while(1){
-            if (programState == DATA_READY){
-                switch (petState){
-                    case EAT_MODE:
-
-                        break;
-
-                    case EXERCISE_MODE:
-                        // MPU power on
-
-                        PIN_setOutputValue(hMpuPin,Board_MPU_POWER, Board_MPU_POWER_ON);
-
-                         // Accelerometer & Gyroscope values: ax,ay,az,gx,gy,gz.
-
-                        //lookinto  acc.ax etc
-                        mpu9250_get_data(&i2cMPU, &ax, &ay, &az, &gx, &gy, &gz);
-                        sprintf(mstr,"Acceleration: ax:%f, ay:%f, az:%f\n", ax,ay,az);
-                        System_printf(mstr);
-                        System_flush();
-
-                        //lookinto  gyro.gx etc
-                        sprintf(mstr,"Gyroscope: gx:%f, gy:%f, gz:%f\n",gx,gy,gz);
-                        System_printf(mstr);
-                        System_flush();
-
-                        // close MPU i2c
-                        I2C_close(i2cMPU);
-
-                        // WAIT 100MS
-                        Task_sleep(100000 / Clock_tickPeriod);
-                        break;
-                    }
-
-                    case PET_MODE:
-
-                        break;
-
-                    }
-
-        Task_sleep(2000000/Clock_tickPeriod);
+    // MPU open i2c
+    i2cMPU = I2C_open(Board_I2C, &i2cMPUParams);
+    if (i2cMPU == NULL) {
+    System_abort("Error Initializing I2CMPU\n");
     }
+
+    // MPU setup and calibration
+    System_printf("MPU9250: Setup and calibration...\n");
+    System_flush();
+
+    mpu9250_setup(&i2cMPU);
+
+    System_printf("MPU9250: Setup and calibration OK\n");
+    System_flush();
+    while (1) {
+
+        if (programState==READ_DATA){
+           // MPU ask data
+            mpu9250_get_data(&i2cMPU, &acc.ax, &acc.ay, &acc.az, &gyro.gx, &gyro.gy, &gyro.gz);
+            if(acc.ax > 0.1){
+               // command_to_send[0]='id: /*sensor id*/, EAT,2';
+                strcpy(command_to_send, "id: 2401, EAT, 2");
+                strcpy(sendingStates, "SEND");
+                sprintf(print_msg, "Current State %s\n", sendingStates);
+                System_printf(print_msg);
+
+            }else if(acc.ay > 0.1){
+
+                strcpy(command_to_send, "id: 2401, PET, 2");
+                strcpy(sendingStates, "SEND");
+                sprintf(print_msg, "Current State %s\n", sendingStates);
+                System_printf(print_msg);
+
+            }else if(acc.az >0.1){
+
+                strcpy(command_to_send, "id: 2401, EXCERCISE, 2");
+                strcpy(sendingStates, "SEND");
+                sprintf(print_msg, "Current State %s\n", sendingStates);
+                System_printf(print_msg);
+            }
+
+            char string[50];
+            sprintf(string,"%d, %d, %d, %d, %d, %d", acc.ax, acc.ay, acc.az, gyro.gx, gyro.gy, gyro.gz);
+            System_printf(string);
+        }
+
+       // Sleep 100ms
+        System_flush();
+        Task_sleep(100000 / Clock_tickPeriod);
+    }
+
+    // MPU close i2c
+    I2C_close(i2cMPU);
+
+    // MPU power off
+    PIN_setOutputValue(hMpuPin,Board_MPU_POWER, Board_MPU_POWER_OFF);
 }
 
 void sensorTaskAmbientLight(UArg arg0, UArg arg1) {
@@ -323,80 +249,74 @@ void sensorTaskAmbientLight(UArg arg0, UArg arg1) {
                 petState= SLEEP_MODE;
             }
 
+            //turn it off
+            //sendingStates =  SEND;
+
+            I2C_close(i2c);
         }
 
         // Just for sanity check for exercise, you can comment this out
         System_printf(" Ambient Light sensorTask\n");
-        // System_flush();
+        System_flush();
         Task_sleep(1000000 / Clock_tickPeriod);
     }
 }
 
-void sensorTaskAccGyro (UArg arg0, UArg arg1) {
-
-    accValues acc;
-    gyroValues gyro;
-
-    //I2C_Transaction i2cMessage;
-    I2C_Handle i2cMPU;
-    I2C_Params i2cMPUParams;
-
-    I2C_Params_init(&i2cMPUParams);
-    i2cMPUParams.bitRate = I2C_400kHz;
-    i2cMPUParams.custom = (uintptr_t)&i2cMPUCfg;
-
-    // MPU power on
-    PIN_setOutputValue(hMpuPin,Board_MPU_POWER, Board_MPU_POWER_ON);
-
-    // Wait 100ms for the MPU sensor to power up
-    Task_sleep(100000 / Clock_tickPeriod);
-    System_printf("MPU9250: Power ON\n");
-    System_flush();
-
-    // MPU open i2c
-    i2cMPU = I2C_open(Board_I2C, &i2cMPUParams);
-    if (i2cMPU == NULL) {
-    System_abort("Error Initializing I2CMPU\n");
-    }
-
-    // MPU setup and calibration
-    System_printf("MPU9250: Setup and calibration...\n");
-    System_flush();
-
-    mpu9250_setup(&i2cMPU);
-
-    System_printf("MPU9250: Setup and calibration OK\n");
-    System_flush();
-    while (1) {
-
-        if (programState==READ_DATA){
-           // MPU ask data
-            mpu9250_get_data(&i2cMPU, &acc.ax, &acc.ay, &acc.az, &gyro.gx, &gyro.gy, &gyro.gz);
-            if(acc.ax > 0.1){
-               // command_to_send[0]='id: /*sensor id*/, EAT,2';
-                strcpy(command_to_send, "id: /*sensor id*/, EAT,2");
-                strcpy(sendingStates, "SEND");
-                sprintf(print_msg, "Current State %s\n", sendingStates);
-                System_printf(print_msg);
-            }
-            char string[50];
-            sprintf(string,"%d, %d, %d, %d, %d, %d", acc.ax, acc.ay, acc.az, gyro.gx, gyro.gy, gyro.gz);
-            System_printf(string);
-        }
-
-       // Sleep 100ms
-        System_flush();
-        Task_sleep(100000 / Clock_tickPeriod);
-    }
-
-    // MPU close i2c
-    I2C_close(i2cMPU);
-
-    // MPU power off
-    PIN_setOutputValue(hMpuPin,Board_MPU_POWER, Board_MPU_POWER_OFF);
+/* Shut Function */
+void shutFxn(PIN_Handle handle, PIN_Id pinId) {
+   PINCC26XX_setWakeup(buttonShut);
+   Power_shutdown(NULL,0);
 }
 
 /*UART Functions*/
+
+/*UART SensorAccGyro Function */
+void uartTaskFxn(UArg arg0, UArg arg1) {
+
+    char echo_msg [30];
+    UART_Handle uart;
+    UART_Params uartParams;
+
+    // Setup here UART connection as 9600,8n1
+    UART_Params_init(&uartParams);
+    uartParams.writeDataMode = UART_DATA_TEXT;
+    uartParams.readDataMode = UART_DATA_TEXT;
+    uartParams.baudRate = 9600;
+    uartParams.readMode = UART_MODE_CALLBACK;
+    //uartParams.readCallback = &uartFxn;
+    uartParams.dataLength = UART_LEN_8;
+    uartParams.parityType = UART_PAR_NONE;
+    uartParams.stopBits = UART_STOP_ONE;
+
+       uart = UART_open(Board_UART0, &uartParams);
+
+          if (uart == NULL) {
+             System_abort("Error opening the UART");
+          }
+
+    while (1) {
+
+        // Print out sensor data as string to debug window if the state is correct
+        // Remember to modify state
+
+        string_compare=strcmp(sendingState, "SEND");
+
+        if (string_compare==0){
+            UART_write(uart,command_to_send, strlen(command_to_send)+1);
+            Task_sleep(100000 / Clock_tickPeriod);
+            strcpy(sendingState, "DETECT");
+        }
+
+        // Just for sanity check for exercise, you can comment this out
+       // System_printf(" uartTask\n");
+        System_flush();
+
+        // Once per second, you can modify this
+        Task_sleep(100000 / Clock_tickPeriod);
+    }
+}
+
+/*UART Function AmbientLight*/
 
 void UARTAmbientLight (UART_Handle uart, UART_Params uartParams){
 
@@ -410,34 +330,21 @@ void UARTAmbientLight (UART_Handle uart, UART_Params uartParams){
     UART_write(uart, echo_msg, strlen(echo_msg));
 }
 
-void UARTAccGyro (UART_Handle uart, UART_Params uartParams){
-    //TODO (gráficas)
-    //ACABAR MIRAR COMO HACERLO ETC
-    char string[50];
-        char echo_msg [30];
-        mpu9250_get_data(&i2cMPU, &acc.ax, &acc.ay, &acc.az, &gyro.gx, &gyro.gy, &gyro.gz);
-                   char string[50];
-                   sprintf(string,"%d", acc.ax);
-                   System_printf(string);
-                   sprintf(string,"%d", acc.ay);
-                   System_printf(string);
-                   sprintf(string,"%d", acc.az);
-                   System_printf(string);
-                   sprintf(string,"%d", gyro.gx);
-                   System_printf(string);
-                   sprintf(string,"%d", gyro.gy);
-                   System_printf(string);
-                   sprintf(string,"%d", gyro.gz);
-                   System_printf(string);
-        sprintf(string,"Acc: x, y, z: %.3f", acc.ax, acc.ay, acc.a);
-        System_printf(string);
-        programState = WAITING;
-        sprintf(echo_msg,"Received: %.3f\n\r",ambientLight);
+//EXTRA
 
-        UART_write(uart, echo_msg, strlen(echo_msg));
+void buttonFxn(PIN_Handle handle, PIN_Id pinId) {
+
+    //?
+    programState = SELECT_BUTTON;
+    if(programState == WAITING){
+        PIN_setOutputValue(ledHandle, Board_LED0, 0 );
+        programState = DATA_READY;
+    }else{
+        PIN_setOutputValue(ledHandle, Board_LED0, 1 );
+        programState = WAITING;
+    }
 }
 
-//EXTRA
 /* Buzz Function */
 
 void simpleBuzzFxn(UArg arg0, UArg arg1) {
@@ -543,7 +450,7 @@ Int main(void) {
     sensorTaskParams.stackSize = STACKSIZE;
     sensorTaskParams.stack = &sensorTaskStack;
     sensorTaskParams.priority=2;
-    sensorTaskHandle1 = Task_create(sensorTaskAmbientLight, &sensorTaskParams, NULL);
+    sensorTaskHandle1 = Task_create(sensorTaskAccGyro, &sensorTaskParams, NULL);
 
     if (sensorTaskHandle1 == NULL) {
         System_abort("Task create failed!");
@@ -573,16 +480,31 @@ Int main(void) {
         System_abort("Task create failed!");
     }
 
-    /* Buzzer Task */
-    Task_Params_init(&buzzerTaskParams);
-    uartTaskParams.stackSize = STACKSIZE;
-    uartTaskParams.stack = &buzzerTaskStack;
-    uartTaskParams.priority=2;
-    uartTaskHandle = Task_create(buzzerTaskFxn, &buzzerTaskParams, NULL);
+    /*UARTAmbientLight*/
 
-    if (buzzerTaskHandle == NULL) {
-        System_abort("Task create failed!");
-    }
+    Task_Params_init(&uartTaskParams);
+        uartTaskParams.stackSize = STACKSIZE;
+        uartTaskParams.stack = &uartTaskStack;
+        uartTaskParams.priority=2;
+        uartTaskHandle = Task_create(UARTAmbientLight, &uartTaskParams, NULL);
+
+        if (uartTaskHandle == NULL) {
+            System_abort("Task create failed!");
+        }
+
+    /*EXTRA*/
+
+    /* Buzzer Task */
+
+        Task_Params_init(&buzzerTaskParams);
+        uartTaskParams.stackSize = STACKSIZE;
+        uartTaskParams.stack = &buzzerTaskStack;
+        uartTaskParams.priority=2;
+        uartTaskHandle = Task_create(buzzerTaskFxn, &buzzerTaskParams, NULL);
+
+        if (buzzerTaskHandle == NULL) {
+            System_abort("Task create failed!");
+        }
 
     /* Sanity check */
     System_printf("Hello world!\n");
